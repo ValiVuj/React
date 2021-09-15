0dfd0864844cc504ef40e27bcd1208711c960ab6
Valentina <valentinaa.vujevic@gmail.com>
2021-09-15 19:55:30 -0400

    This is my first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import * as dayjs  from "dayjs";
+import { nanoid } from "nanoid";
+
+console.log(dayjs().format());
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Ciao Dzimba</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world
         </p>
+        <p>how you doin today is {dayjs().format("MMMM/DD/YYYY, HH:mm:ss")}</p>
+        <p>Your unique ID is {nanoid()}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
